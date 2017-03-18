18c34d7fab1b50bf880d07edae45c3abd95cdbef
Marcin Matłacz <marcin@matlacz.eu>
2017-03-18 12:36:53 +0100

    Full skyscanner flights support

---
+++ data_provider.py
@@ -0,0 +1,126 @@
+import json
+import time
+from datetime import datetime, timedelta
+
+import pandas as pd
+import requests
+
+API_KEY = 'ma595491219569679758263226220714'
+
+
+class DataProvider:
+    def __init__(self, locale, currency, country, origin,
+                 outbound_date, inbound_date):
+        self._quotes_url = 'http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/{}/{}/{}/{}/{}/{}/{}\
+        ?apiKey={}'
+        self._codes_table = pd.DataFrame()
+        self._locale = locale
+        self._currency = currency
+        self._country = country
+        self._origin = origin
+        self._outbound_date = outbound_date
+        self._inbound_date = inbound_date
+
+    def _create_url(self, destination='anywhere'):
+        return self._quotes_url.format(
+            self._country,
+            self._currency,
+            self._locale,
+            self._origin,
+            destination,
+            self._outbound_date,
+            self._inbound_date,
+            API_KEY,
+        )
+
+    @staticmethod
+    def get_suggestions(city):
+        url = 'http://partners.api.skyscanner.net/apiservices/autosuggest/v1.0/CH/CHF/en-GB/?query={}&apiKey={}'.format(
+            city, API_KEY)
+        suggestions = requests.get(url).json()
+        suggestions = list(
+            map(lambda x: {'name': x['PlaceName'], 'code': x['PlaceId']},
+                suggestions['Places']))
+        return suggestions
+
+    def _get_connections(self, destination='anywhere'):
+        data = requests.get(self._create_url(destination))
+        return data.json()
+
+    def _select_col_places(self, data):
+        return pd.DataFrame(data['Places'])
+
+    def _select_col_quotes(self, data):
+        return pd.DataFrame(data['Quotes'])
+
+    def _get_names(self, df):
+        return df['OutboundLeg'].map(
+            lambda x: {
+                'origin': self._get_airport_name(x['OriginId']),
+                'destination': self._get_airport_name(x['DestinationId'])
+            }
+        )
+
+    def _get_airport_name(self, id_):
+        airport_info = self._codes_table[self._codes_table['PlaceId'] == id_]
+        return {
+            'city_name': airport_info['CityName'].values[-1],
+            'country_name': airport_info['CountryName'].values[-1],
+            'code': airport_info['SkyscannerCode'].values[-1]
+        }
+
+    def get_propositions(self, destination='anywhere'):
+        data = self._get_connections(destination)
+        quotes = self._select_col_quotes(data)
+        self._codes_table = self._select_col_places(data)
+        airports_names = self._get_names(quotes)
+        quotes['Airport'] = airports_names
+        return quotes
+
+    def _get_session(self, origin, destination, outdate, indate, adults):
+        url = "http://partners.api.skyscanner.net/apiservices/pricing/v1.0"
+
+        payload = "apiKey={}&country={}&currency={}&locale={}&originplace={}-sky&destinationplace={}-sky\
+        &outbounddate={}&inbounddate={}&adults={}"
+        payload = payload.format(
+            API_KEY,
+            self._country,
+            self._currency,
+            self._locale,
+            origin,
+            destination,
+            outdate,
+            indate,
+            adults,
+        )
+        headers = {
+            'content-type': "application/x-www-form-urlencoded",
+            'accept': "application/json",
+            'cache-control': "no-cache",
+        }
+
+        response = requests.request("POST", url, data=payload, headers=headers)
+
+        return response.headers['Location']
+
+    def get_prices(self, origin, destination, outdate, indate, adults):
+        session_url = self._get_session(
+            origin,
+            destination,
+            outdate,
+            indate,
+            adults,
+        )
+        query = '?sortorder={}&apiKey={}'.format('asc', API_KEY)
+        response = requests.get(session_url + query)
+        while response.status_code == '204':
+            time.sleep(5)
+            response = requests.get(session_url + query)
+        return response.json()
+
+
+if __name__ == '__main__':
+    dp = DataProvider('en-GB', 'CHF', 'CH', DataProvider.get_suggestions('Warszawa')[1]['code'],
+                      (datetime.today() + timedelta(days=2)).date(),
+                      (datetime.today() + timedelta(days=7)).date())
+    print(json.dumps(dp.get_propositions(destination=DataProvider.get_suggestions('Zurych')[-1]['code'])))
---
+++ skyscanner_live_pricing.py
@@ -0,0 +1,127 @@
+import time
+from copy import deepcopy
+from datetime import datetime, timedelta
+
+import requests
+
+from data_provider import DataProvider
+
+API_KEY = 'ma595491219569679758263226220714'
+
+
+class LivePricing:
+    def __init__(self, origin, destination, start_date, end_date, adults,
+                 market='CH', currency='CHF', locale='en-GB'):
+        data = {'apiKey': API_KEY, 'country': market, 'currency': currency,
+                'locale': locale, 'originplace': origin + '-sky',
+                'destinationplace': destination + '-sky',
+                'outbounddate': start_date, 'inbounddate': end_date,
+                'adults': adults}
+        url = 'http://partners.api.skyscanner.net/apiservices/pricing/v1.0'
+        r = requests.post(url, data)
+        while r.status_code == '204':
+            time.sleep(5)
+            r = requests.post(url, data)
+        self.get_url = r.headers[
+                           'Location'] + '?apiKey=' + API_KEY + '&pagesize=1'
+
+    def _get_cheapest(self):
+        time.sleep(5)
+        r = requests.get(self.get_url)
+        while r.status_code == '204':
+            time.sleep(5)
+            r = requests.get(self.get_url)
+        try:
+            json_data = r.json()
+        except Exception:
+            return []
+        return json_data
+
+    @staticmethod
+    def _del_keys(d):
+        copy = deepcopy(d)
+        for key in d.keys():
+            if key not in {'Carriers', 'arrival_date', 'arrival_time', 'time',
+                           'date', 'Duration'}:
+                del copy[key]
+        return copy
+
+    def _process_data(self, c, details, agents_mapping, carriers_mapping):
+        inbound = details[c['InboundLegId']]
+        outbound = details[c['OutboundLegId']]
+
+        inbound['date'] = datetime.strptime(inbound['Departure'],
+                                            '%Y-%m-%dT%H:%M:%S').date()
+        outbound['date'] = datetime.strptime(outbound['Departure'],
+                                             '%Y-%m-%dT%H:%M:%S').date()
+        inbound['time'] = datetime.strptime(inbound['Departure'],
+                                            '%Y-%m-%dT%H:%M:%S').time()
+        outbound['time'] = datetime.strptime(outbound['Departure'],
+                                             '%Y-%m-%dT%H:%M:%S').time()
+
+        inbound['arrival_date'] = datetime.strptime(inbound['Arrival'],
+                                                    '%Y-%m-%dT%H:%M:%S').date()
+        outbound['arrival_date'] = datetime.strptime(outbound['Arrival'],
+                                                     '%Y-%m-%dT%H:%M:%S').date()
+        inbound['arrival_time'] = datetime.strptime(inbound['Arrival'],
+                                                    '%Y-%m-%dT%H:%M:%S').time()
+        outbound['arrival_time'] = datetime.strptime(outbound['Arrival'],
+                                                     '%Y-%m-%dT%H:%M:%S').time()
+
+
+        try:
+            inbound['Carriers'] = [carriers_mapping[x] for x in
+                                   inbound['Carriers']]
+        except KeyError:
+            pass
+
+        try:
+            outbound['Carriers'] = [carriers_mapping[x] for x in
+                                    outbound['Carriers']]
+        except KeyError:
+            pass
+
+        c['InboundDetails'] = self._del_keys(inbound)
+        c['OutboundDetails'] = self._del_keys(outbound)
+
+        del c['OutboundLegId']
+        del c['InboundLegId']
+        del c['BookingDetailsLink']
+        for x in c['PricingOptions']:
+            x['Agents'] = [agents_mapping[p] for p in x['Agents']]
+        return c
+
+    def _parse_data(self, cheapest):
+        carriers_mapping = {x['Id']: (x['Name'], x['ImageUrl']) for x in
+                            cheapest['Carriers']}
+        agents_mapping = {c['Id']: c['ImageUrl'] for c in cheapest['Agents']}
+        details = {c['Id']: c for c in cheapest['Legs']}
+        results = []
+        for c in cheapest['Itineraries']:
+            results.append(self._process_data(
+                c, details=details,
+                agents_mapping=agents_mapping,
+                carriers_mapping=carriers_mapping)
+            )
+        return results
+
+    def find_flights(self):
+        """
+
+        :return: List of flights with details
+        """
+        cheapest = self._get_cheapest()
+        return self._parse_data(cheapest)
+
+
+if __name__ == '__main__':
+    outbound = (datetime.today() + timedelta(days=2)).date()
+    inbound = (datetime.today() + timedelta(days=7)).date()
+
+    cheapest = LivePricing(
+        DataProvider.get_suggestions('Warszawa')[1]['code'].split('-')[0],
+        DataProvider.get_suggestions('Zurych')[-1]['code'].split('-')[0],
+        outbound,
+        inbound,
+        1).find_flights()
+    print(cheapest)
