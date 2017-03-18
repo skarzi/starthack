3405f7f159c254d41f6c9ddfa76b3a5b8b1b6540
Łukasz Skarżyński <skarzynski_lukasz@protonmail.com>
2017-03-18 19:17:01 +0100

    Change weather api to gather data from many days

--- open_weather_map_facade.py
+++ open_weather_map_facade.py
@@ -1,28 +1,30 @@
 import requests
 
-API_URL = 'http://api.openweathermap.org/data/2.5/weather'
+API_URL = 'http://api.openweathermap.org/data/2.5/forecast/daily'
 API_KEY = '4c7fd0781b820a77cdf429f35ad92599'
-ICON_STORE = '/tmp/'
 
 
 class OpenWeatherManFacade:
     def get_weather(self, city):
-        weather_info = requests.get(
+        weather_infos = requests.get(
             API_URL,
             params={
                 'APPID': API_KEY,
                 'q': city,
                 'units': 'metric',
             },
-        ).json()
-        icons_name = [x['icon'] for x in weather_info['weather']]
-        return {
-                'temp': weather_info['main']['temp'],
-                'pressure': weather_info['main']['pressure'],
-                'humidity': weather_info['main']['humidity'],
+        ).json()['list']
+        result = list()
+        for weather_info in weather_infos:
+            icons_name = [x['icon'] for x in weather_info['weather']]
+            result.append({
+                'temp': weather_info['temp'],
+                'pressure': weather_info['pressure'],
+                'humidity': weather_info['humidity'],
                 'description': [x['description'] for x in weather_info['weather']],
                 'icons': self._get_icon(icons_name),
-        }
+            })
+        return result
 
     def _get_icon(self, icons_name):
         return ['http://openweathermap.org/img/w/{name}.png'.format(
